b612c3082a720c29004a875fad184946ccfe0d6b
tuanndhust <nguyentuan2701hd@gmail.com>
2022-05-04 22:21:26 +0700

    Done assignment1_1,1_2

--- Assignment1_2.py
+++ Assignment1_2.py
@@ -2,20 +2,39 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-#img = cv.imread('/home/tuannghust/ImagesProcessing/ImagesProcessing/Lena.png',0)
 def Histogram(img,bin):
     bin1 = np.arange(bin+1)
-    print(bin1)
-    imgs = cv.imread(img,0)
-    print(imgs.shape)
-    imgss = imgs.flatten()
+    imgs = cv.imread(img)
+    #print(imgs)
+    #print(imgs.shape)
+    #print(imgs[:,:,2])
+    #number channels of images
+    dims = imgs.ndim
+    if dims == 1:
+        imgss = imgs.flatten()
     #print(type(img))
     # for i in range(bin+1):
     #     bin1.append(i)
-    fig,ax = plt.subplots(1,1)
-    ax.hist(imgss,bins=bin1)
-    ax.set_title('Histogram of input image')
+        fig,ax = plt.subplots(1,1)
+        ax.hist(imgss,bins=bin1)
+        ax.set_title('Histogram of input image')
+    elif dims == 3:
+        #blue channel
+        fig, (ax, ax1, ax2) = plt.subplots(1,3)
+        b = imgs[:,:,0]
+        bs = b.flatten()
+        ax.hist(bs,bins=bin1)
+        ax.set_title('blue')  
+        #grown channel
+        g = imgs[:,:,1]
+        gs = g.flatten() 
+        ax1.hist(gs,bins=bin1)
+        ax1.set_title('grown')
+        #red channel
+        r = imgs[:,:,2]
+        rs = r.flatten() 
+        ax2.hist(rs,bins=bin1)
+        ax2.set_title('red')    
     plt.show()    
 
-Histogram('/home/tuanhust/ImagesProcessing/Lena.png',255)
-
+Histogram('/home/tuannghust/ImagesProcessing/ImagesProcessing/Lena.png',255)
