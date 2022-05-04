f5234d170267266f6dccab4eb2c5af90224d7164
tuanndhust <nguyentuan2701hd@gmail.com>
2022-05-04 22:32:37 +0700

    write image

--- Assignment1_1.py
+++ Assignment1_1.py
@@ -2,9 +2,14 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 #read an image from dictionary
-img = cv.imread('/home/tuanhust/ImagesProcessing/Lena.png')
+img = cv.imread('/home/tuannghust/ImagesProcessing/ImagesProcessing/Lena.png')
 #show image on a window
 cv.imshow('Lena',img)
-# plt.hist(img.ravel(),256,[0,256]); plt.show()
+#replace blue channel of image
+img[:,:,0] = 0
+cv.imwrite('Lena_blue.png', img)
+
+
+
 cv.waitKey(0)
 cv.destroyAllWindows()
