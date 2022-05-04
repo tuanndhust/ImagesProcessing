0135d3d752af10a0a887827b2adf2a1aef5e8bce
tuanndhust <nguyentuan2701hd@gmail.com>
2022-05-04 15:46:24 +0700

    changes Assignment1-2

--- Assignment1_1.py
+++ Assignment1_1.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 #read an image from dictionary
-img = cv.imread('/home/tuannghust/ImagesProcessing/ImagesProcessing/Lena.png')
+img = cv.imread('/home/tuanhust/ImagesProcessing/Lena.png')
 #show image on a window
 cv.imshow('Lena',img)
 # plt.hist(img.ravel(),256,[0,256]); plt.show()
