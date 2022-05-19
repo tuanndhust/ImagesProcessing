25ae9c5b64ffb681e5dd38c15b04de30c2ee0190
tuanndhust <nguyentuan2701hd@gmail.com>
2022-05-19 15:57:51 +0700

    doing multi matrix

--- test.py
+++ test.py
@@ -3,21 +3,25 @@ import numpy as np
 ego = '/home/tuannghust/ego2odo.txt'
 #path to converted file
 odo = '/home/tuannghust/odo/txt'
+#from list string to transformation matrix (4x4)
 def str2arr(str):
     arr = str.split(" ")
     arr = list(map(int, arr))
     arr = np.array(arr)
     arr = arr.reshape([3,4])
+    arr1 = np.array([[0,0,0,1]])
+    arr = np.concatenate((arr,arr1))
     #print(arr)
     return arr
 with open(ego) as f:
     read_pose = [s.strip() for s in f.readlines()]
     num_pose = len(read_pose)
-ar = str2arr(read_pose[1]) 
-ar2 =  str2arr(read_pose[1-1])
-print(ar.dot(ar2))
-print(ar)
-print(ar2.shape)
-# for i in range(1,num_pose):
-#      read_pose[i] = str2arr(read_pose[i]) @ str2arr(read_pose[i-1])
-# print(read_pose)
+# ar = str2arr(read_pose[1]) 
+# ar2 =  str2arr(read_pose[1-1])
+# print(ar.dot(ar2))
+# print(ar)
+# print(ar2.shape)
+for i in range(1,num_pose):
+    read_pose[i] = str2arr(read_pose[i]) @ str2arr(read_pose(i-1))
+    
+print(read_pose)
