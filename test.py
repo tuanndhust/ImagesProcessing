ba36e8529e3eeeedb70b954986079c50df2d8cac
tuanndhust <nguyentuan2701hd@gmail.com>
2022-05-21 10:21:47 +0700

    visualize

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
-a = ['1','2','3']
-b = []
-for i in range(3):
-    a[i] = int(a[i])
-    b.append(a[i])
-print(b)
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.load("/home/tuannghust/SfmLearner-Pytorch/SfmLearner Models-20220520T152108Z-001/SfmLearner Models/predictions.npy")
+for i in range(1,len(data)):
+	r = data[i-1,1]
+	data[i] = r[:,:3] @ data[i]
+	data[i,:,:,-1] = data[i,:,:,-1] + r[:,-1]
+groundtruth = []
+for i in range(0, 5):
+    x = data[0, i, 0, 3]
+    z = data[0, i, 2, 3]
+    groundtruth = np.append(groundtruth, [x, z])
+for i in range(1, 1201):
+    x = data[i, 4, 0, 3]
+    z = data[i, 4, 2, 3]
+    groundtruth = np.append(groundtruth, [x, z])
+g = groundtruth.reshape(1205, 2)
+plt.scatter(g[:, 0], g[:, 1])
+plt.savefig('/home/tuannghust/10_1.jpg')
